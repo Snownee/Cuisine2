67f66bcc6707d77aa1de34743c3d047c7fd37487
Snownee <1850986885@qq.com>
2020-02-01 02:54:43 +0800

    Fix filling recipe, pt. 1

--- src/main/java/snownee/cuisine/base/crafting/SpiceBottleFillingRecipe.java
+++ src/main/java/snownee/cuisine/base/crafting/SpiceBottleFillingRecipe.java
@@ -1,6 +1,6 @@
 package snownee.cuisine.base.crafting;
 
-import java.util.Optional;
+import java.util.stream.Stream;
 
 import javax.annotation.Nullable;
 
@@ -10,108 +10,85 @@ import com.google.gson.JsonSyntaxException;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.ICraftingRecipe;
 import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.JSONUtils;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.ForgeRegistryEntry;
 import snownee.cuisine.api.CuisineAPI;
-import snownee.cuisine.api.registry.Spice;
 import snownee.cuisine.base.BaseModule;
 import snownee.cuisine.base.item.SpiceBottleItem;
+import snownee.kiwi.crafting.DynamicShapedRecipe;
 import snownee.kiwi.util.Util;
 
-public class SpiceBottleFillingRecipe implements ICraftingRecipe {
-    private final ResourceLocation id;
-    private final String group;
+public class SpiceBottleFillingRecipe extends DynamicShapedRecipe {
     private final SpiceBottleItem bottle;
 
     public SpiceBottleFillingRecipe(ResourceLocation id, String group, SpiceBottleItem bottle) {
-        this.id = id;
-        this.group = group;
+        super(id, group, 1, 2, NonNullList.from(Ingredient.EMPTY, SpiceIngredient.INSTANCE, Ingredient.fromItems(bottle)), ItemStack.EMPTY);
         this.bottle = bottle;
     }
 
     @Override
-    public boolean matches(CraftingInventory inv, World worldIn) {
-        for (int x = 0; x < inv.getWidth(); x++) {
-            for (int y = 0; y < inv.getHeight() - 1; y++) {
-                if (checkRecipe(inv, x + y * inv.getWidth())) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean checkRecipe(CraftingInventory inv, int slot) {
-        ItemStack spice = inv.getStackInSlot(slot);
-        Optional<Spice> spice_op = CuisineAPI.findSpice(spice);
-        if (!spice_op.isPresent()) {
+    public boolean matches(CraftingInventory inv, World world) {
+        int[] pos = getMatchPos(inv);
+        if (pos == null) {
             return false;
         }
-        ItemStack bottle = inv.getStackInSlot(slot + inv.getWidth());
-        return bottle.getItem() == this.bottle && this.bottle.fill(bottle, spice, IFluidHandler.FluidAction.SIMULATE) != 0;
+        ItemStack in = inv.getStackInSlot(pos[0] + pos[1] * inv.getWidth());
+        ItemStack container = inv.getStackInSlot(pos[0] + (pos[1] + 1) * inv.getWidth());
+        return bottle.fill(container, in, IFluidHandler.FluidAction.SIMULATE) > 0;
     }
 
     @Override
     public ItemStack getCraftingResult(CraftingInventory inv) {
-        ItemStack bottle = ItemStack.EMPTY;
-        ItemStack spice = ItemStack.EMPTY;
-        for (int k = 0; k < inv.getSizeInventory(); ++k) {
-            ItemStack stack = inv.getStackInSlot(k);
-            if (!stack.isEmpty()) {
-                if (stack.getItem() == this.bottle) {
-                    bottle = stack.copy();
-                    bottle.setCount(1);
-                } else {
-                    spice = stack;
-                }
-            }
-        }
-        if (bottle.isEmpty() || spice.isEmpty()) {
+        int[] pos = getMatchPos(inv);
+        if (pos == null) {
             return ItemStack.EMPTY;
         }
-        int num = this.bottle.fill(bottle, spice, IFluidHandler.FluidAction.EXECUTE);
-        if (num == 0) {
-            return ItemStack.EMPTY;
-        }
-        spice.shrink(num - 1);
-        return bottle;
+        ItemStack in = inv.getStackInSlot(pos[0] + pos[1] * inv.getWidth());
+        ItemStack container = inv.getStackInSlot(pos[0] + (pos[1] + 1) * inv.getWidth()).copy();
+        int num = this.bottle.fill(container, in, IFluidHandler.FluidAction.EXECUTE);
+        return num == SpiceBottleItem.VOLUME_PER_ITEM ? container : ItemStack.EMPTY;
     }
 
     @Override
-    public boolean isDynamic() {
-        return true;
-    }
-
-    @Override
-    public boolean canFit(int width, int height) {
-        return width > 0 && height > 1;
-    }
-
-    @Override
-    public ItemStack getRecipeOutput() {
-        return ItemStack.EMPTY;
-    }
-
-    @Override
-    public ResourceLocation getId() {
-        return id;
-    }
-
-    @Override
-    public String getGroup() {
-        return group;
+    public NonNullList<ItemStack> getRemainingItems(CraftingInventory inv) {
+        return NonNullList.withSize(inv.getSizeInventory(), ItemStack.EMPTY);
     }
 
     @Override
     public IRecipeSerializer<?> getSerializer() {
         return BaseModule.SPICE_BOTTLE_FILL;
+    }
+
+    private static class SpiceIngredient extends Ingredient {
+
+        public static final SpiceIngredient INSTANCE = new SpiceIngredient();
+
+        private SpiceIngredient() {
+            super(Stream.empty());
+        }
+
+        @Override
+        public boolean isSimple() {
+            return false;
+        }
+
+        @Override
+        public boolean hasNoMatchingItems() {
+            return false; // Currently spices may not be loaded.
+        }
+
+        @Override
+        public boolean test(ItemStack stack) {
+            return CuisineAPI.findSpice(stack).isPresent();
+        }
     }
 
     public static class Serializer extends ForgeRegistryEntry<IRecipeSerializer<?>> implements IRecipeSerializer<SpiceBottleFillingRecipe> {
@@ -141,7 +118,7 @@ public class SpiceBottleFillingRecipe implements ICraftingRecipe {
 
         @Override
         public void write(PacketBuffer buffer, SpiceBottleFillingRecipe recipe) {
-            buffer.writeString(recipe.group);
+            buffer.writeString(recipe.getGroup());
             buffer.writeRegistryIdUnsafe(ForgeRegistries.ITEMS, recipe.bottle);
         }
 
