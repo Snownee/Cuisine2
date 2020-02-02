20f999acabd8158ac256f892360a390aff7bea86
jihuayu <8042833@qq.com>
2020-02-02 15:36:05 +0800

    clean code.

--- src/main/java/snownee/cuisine/farm/FarmModule.java
+++ src/main/java/snownee/cuisine/farm/FarmModule.java
@@ -1,21 +1,8 @@
 package snownee.cuisine.farm;
 
-import com.google.common.collect.Sets;
-import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.material.Material;
-import net.minecraft.item.crafting.IRecipeSerializer;
-import net.minecraft.tileentity.TileEntityType;
-import snownee.cuisine.base.block.SpiceRackBlock;
-import snownee.cuisine.base.crafting.SpiceBottleFillingRecipe;
-import snownee.cuisine.base.item.SpiceBottleItem;
-import snownee.cuisine.base.tile.SpiceRackTile;
 import snownee.kiwi.AbstractModule;
 import snownee.kiwi.KiwiModule;
-import snownee.kiwi.Name;
-import snownee.kiwi.RenderLayer;
-
-import java.util.Set;
 
 @KiwiModule(name = "farm", dependencies = "@core")
 @KiwiModule.Group("decorations")
